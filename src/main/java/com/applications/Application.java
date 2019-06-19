5514831f40b52acf585bb6167cb136ffcf43b2a2
pachyderme <bouvetpierre49@gmail.com>
2019-06-19 15:01:09 +0200

    user's connection

--- src/main/java/com/applications/Application.java
+++ src/main/java/com/applications/Application.java
@@ -1,20 +1,72 @@
 package com.applications;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Application {
+	private static UsersManager usersManager;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
-		System.out.println("Saisissez votre nom :");
-        String name = System.console().readLine();
-        User connectedUser = connection(name);
-        if(connectedUser != null) {
-        	System.out.println("Bienvenue "+ name);
-        }
+		connection();
+    	handleCommands();
 	}
 	
-	public static User connection(String name) {
-		return new User(name);
+	public static void connection() {
+		usersManager = new UsersManager();
+		usersManager.createAdminIfNotExists();
+				
+		String userName = "";
+		User user = null;
+		boolean knownUser = false;
+		while(!knownUser) {
+			System.out.println("Saisissez votre nom :");
+	        userName = askString();
+	        
+	        user = new User(userName);
+	        knownUser = usersManager.userExists(user);
+	        
+	        if(!knownUser) {
+	    		System.out.println("Utilisateur incconu");
+	        }
+		}
+
+    	System.out.println("Bienvenue "+ userName);
+	}
+	
+	private static void handleCommands() {
+		String choice = null;
+
+		while (!"exit".equals(choice)) {
+			System.out.println(" > ");
+			choice = askString();
+			switch (choice) {
+				case "adduser":
+					System.out.println("Nom de l'utilisateur : ");
+					String userName = askString();
+					usersManager.createUser(userName);
+					System.out.println("Utilisateur créé.");
+					break;
+				case "exit":
+					System.out.println("Fermeture de l'application.");
+					break;
+				default:
+					System.out.println("Commande inconnue");
+			}
+		}
+	}
+	
+	
+	private static String askString() {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		try {
+			str = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return str;		
 	}
 	
 }
---
+++ src/test/java/com/applications/UsersManagerTests.java
@@ -0,0 +1,95 @@
+package com.applications;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsersManagerTests {
+	private UsersManager usersManager;
+	
+	@Before
+	public void setup () {
+		usersManager = new UsersManager();
+		usersManager.deleteUsersFile();
+	}
+	
+	@Test
+	public void adminExists() {
+		usersManager.deleteUsersFile();
+		
+		boolean adminExists = usersManager.adminExists();
+		Assert.assertFalse(adminExists);
+	}
+	
+	@Test
+	public void userNotExists() {
+		usersManager.deleteUsersFile();
+		
+		User user = new User("Kevin");
+		boolean userExists = usersManager.userExists(user);
+		
+		Assert.assertFalse(userExists);
+	}
+	
+	@Test
+	public void userExists() {
+		usersManager.deleteUsersFile();
+		
+		usersManager.createAdminIfNotExists();
+		User user = new User("Admin");
+
+		boolean userExists = usersManager.userExists(user);
+		Assert.assertTrue(userExists);
+	}
+	
+	@Test
+	public void adminCreated() {
+		usersManager.deleteUsersFile();
+		
+		usersManager.createAdminIfNotExists();
+
+		boolean adminExists = usersManager.adminExists();
+		Assert.assertTrue(adminExists);
+	}
+
+	@Test
+	public void getUsersEmpty() {
+		usersManager.deleteUsersFile();
+
+		ArrayList<String> users = usersManager.getUsersFromFile();
+		
+		Assert.assertTrue(users.isEmpty());
+	}
+
+	@Test
+	public void getUsersFilledWithAdmin() {
+		usersManager.deleteUsersFile();
+		
+		usersManager.createAdminIfNotExists();
+		
+		ArrayList<String> users = usersManager.getUsersFromFile();
+		
+		Assert.assertFalse(users.isEmpty());
+	}
+
+	@Test
+	public void getUsersFilledWithUser() {
+		usersManager.deleteUsersFile();
+
+		usersManager.createUser("Pierre");
+		usersManager.createUser("Paul");
+		usersManager.createUser("Jacques");
+		
+		ArrayList<String> users = usersManager.getUsersFromFile();
+		
+		Assert.assertEquals(users.size(), 3);
+	}
+	
+	@After
+	public void finish () {
+		usersManager.deleteUsersFile();
+	}
+}
