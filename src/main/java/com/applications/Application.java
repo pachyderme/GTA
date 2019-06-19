bfcedf28813c9219bbef5516b746019f12577339
Kevin Pichon <kevinpichon@outlook.fr>
2019-06-19 15:58:25 +0200

    Add CheckStyle

--- src/main/java/com/applications/Application.java
+++ src/main/java/com/applications/Application.java
@@ -5,68 +5,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Application {
-	private static UsersManager usersManager;
+    private static UsersManager usersManager;
 
-	public static void main(String[] args) {
-		connection();
-    	handleCommands();
-	}
-	
-	public static void connection() {
-		usersManager = new UsersManager();
-		usersManager.createAdminIfNotExists();
-				
-		String userName = "";
-		User user = null;
-		boolean knownUser = false;
-		while(!knownUser) {
-			System.out.println("Saisissez votre nom :");
-	        userName = askString();
-	        
-	        user = new User(userName);
-	        knownUser = usersManager.userExists(user);
-	        
-	        if(!knownUser) {
-	    		System.out.println("Utilisateur incconu");
-	        }
-		}
+    public static void main(String[] args) {
+        connection();
+        handleCommands();
+    }
+    
+    public static void connection() {
+        usersManager = new UsersManager();
+        usersManager.createAdminIfNotExists();
+                
+        String userName = "";
+        User user = null;
+        boolean knownUser = false;
+        while (!knownUser) {
+            System.out.println("Saisissez votre nom :");
+            userName = askString();
+            
+            user = new User(userName);
+            knownUser = usersManager.userExists(user);
+            
+            if (!knownUser) {
+                System.out.println("Utilisateur incconu");
+            }
+        }
 
-    	System.out.println("Bienvenue "+ userName);
-	}
-	
-	private static void handleCommands() {
-		String choice = null;
+        System.out.println("Bienvenue " + userName);
+    }
+    
+    private static void handleCommands() {
+        String choice = null;
 
-		while (!"exit".equals(choice)) {
-			System.out.println(" > ");
-			choice = askString();
-			switch (choice) {
-				case "adduser":
-					System.out.println("Nom de l'utilisateur : ");
-					String userName = askString();
-					usersManager.createUser(userName);
-					System.out.println("Utilisateur créé.");
-					break;
-				case "exit":
-					System.out.println("Fermeture de l'application.");
-					break;
-				default:
-					System.out.println("Commande inconnue");
-			}
-		}
-	}
-	
-	
-	private static String askString() {
+        while (!"exit".equals(choice)) {
+            System.out.println(" > ");
+            choice = askString();
+            switch (choice) {
+                case "adduser":
+                    System.out.println("Nom de l'utilisateur : ");
+                    String userName = askString();
+                    usersManager.createUser(userName);
+                    System.out.println("Utilisateur créé.");
+                    break;
+                case "exit":
+                    System.out.println("Fermeture de l'application.");
+                    break;
+                default:
+                    System.out.println("Commande inconnue");
+            }
+        }
+    }
+    
+    
+    private static String askString() {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = null;
-		try {
-			str = br.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return str;		
-	}
-	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;        
+    }
+    
 }
--- src/main/java/com/applications/User.java
+++ src/main/java/com/applications/User.java
@@ -1,34 +1,34 @@
 package com.applications;
 
 public class User {
-	public String type;
-	public String name;
-	public int id;
+    public String type;
+    public String name;
+    public int id;
 
-	public User(String nameSelected) {
-		this.setName(nameSelected);
-	}
-	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getId() {
-		return id;
-	}
+    public User(String nameSelected) {
+        this.setName(nameSelected);
+    }
+    
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	
+    public void setId(int id) {
+        this.id = id;
+    }
+    
 }
